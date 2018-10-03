af6cc6268f02abbb0dfb91483d3c3ec8c67f66eb
Kaitlin McTigue <kaitlinmctigue@Kaitlins-MBP.wv.cc.cmu.edu>
2018-10-03 11:47:26 -0400

    clean up

--- week6_jquery/main.js
+++ week6_jquery/main.js
@@ -1,10 +1,8 @@
 
-
 $(document).on("click", "#add-item", function() {
     var list = $("#grocery-list");
     var itemInput = $("#new-list-item");
 
-    console.log(itemInput);
     if (itemInput.val()) {
         list.append("<li>" + "<input type=\"checkbox\" class=\"complete-item\">" + itemInput.val() + " <button class=\"delete-item\">X</button>" + "</li>");
     }
@@ -16,11 +14,9 @@ $(document).on("click", ".delete-item", function() {
 });
 
 $(document).on("click", ".complete-item", function() {
-    if (this.checked) { // true if the input checkbox is checked
+    if (this.checked) {
         this.parentNode.style.textDecoration = "line-through";
-        // in css, this would be: "text-decoration: line-through"
     } else {
         this.parentNode.style.textDecoration = "none";
-        // in css, this would be: "text-decoration: none"
     }
 });
